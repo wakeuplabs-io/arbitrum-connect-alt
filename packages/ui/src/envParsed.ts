fd5b6bd9a02b5048917e55b2c8f1a4eb7f1295b4
Christian Escalante <chescalante.ar@gmail.com>
2025-06-13 11:02:27 -0300

    deploy one

--- packages/ui/src/envParsed.ts
+++ packages/ui/src/envParsed.ts
@@ -3,13 +3,11 @@ import { z } from "zod";
 // NOTE: DO NOT destructure import.meta.env
 
 const env = {
-  APP_URL: import.meta.env.VITE_APP_URL,
   API_URL: import.meta.env.VITE_API_URL,
 };
 
 const envSchema = z
   .object({
-    APP_URL: z.string().url().optional().default("http://localhost:3000"),
     API_URL: z.string().url().optional().default("http://localhost:9999"),
   })
   .required();
